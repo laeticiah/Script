ff0882489fee07eb4d31be5ac8e271f53d19d343
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-04-05 13:00:18 -0700

    Create env_manager.py

---
+++ lib/env_manager.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+def load_env_var(var_name:str):
+    """
+    Load an environment variable and return its value.
+
+    Args:
+        var_name : Name of environment variable to load.
+
+    Returns:
+        Environment variable value
+
+    Raises:
+        ValueError: If no environment variable exists with the given name.
+    """
+    load_dotenv()
+    var_value = os.getenv(var_name)
+
+    if not var_value:
+        logger.error(f'Environment variable {var_name} is required')
+        raise ValueError(f'Environment variable {var_name} is required')
+
+    return var_value
