f29df58966cd2dd320029075f4de1c0bd462cdb6
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-01 08:26:52 -0700

    Update java_parser.py

--- lib/parsers/java_parser.py
+++ lib/parsers/java_parser.py
@@ -27,17 +27,6 @@ def parse_java_file(file_content: str) -> dict:
     result = {}
     service_matches = re.findall(SERVICE_PATTERN, file_content)
 
-    # try:
-    #     for service_name, service_class in service_matches:
-    #         function_regex = re.compile(FUNCTION_PATTERN % service_name)
-    #         function_matches = function_regex.findall(file_content)
-    #         result[service_class] = []
-    #         for match in function_matches:
-    #             if match.startswith(('run', 'create', 'delete', 'put', 'modify', 'register', 'update')):
-    #                 result[service_class].append(match)
-    # except Exception as e:   # pylint: disable=broad-exception-caught
-    #     logger.error("Error parsing Javas file: %s", str(e))
-    #     return {}
     
     try:
         for match in service_matches:
@@ -55,16 +44,3 @@ def parse_java_file(file_content: str) -> dict:
         return {}
 
     return result
-
-# Example usage
-# java_code = '''
-#     AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
-#     s3.createBucket("bucketName");
-#     s3.putObject("bucketName", "key", "content");
-
-#     AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
-#     ec2.runInstances(new RunInstancesRequest());
-# '''
-
-# result = parse_java_file(java_code)
-# print(result)
