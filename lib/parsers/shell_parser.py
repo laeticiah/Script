2a5591c70cfd8e548bbbfeeef17f0d2119503ebd
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-01 09:02:26 -0700

    Update shell_parser.py

--- lib/parsers/shell_parser.py
+++ lib/parsers/shell_parser.py
@@ -3,16 +3,25 @@ Shell parser
 '''
 import re
 
-
 def parse_shell_file(file_content: str):
     resource_creations = []
 
-    PATTERN = r'\baws\s+\w+\s+(\b(create|put|run|make|build|launch)\b)(-\w+)+'
-    for line in file_content.split('\n'):
-        search = re.search(PATTERN, line, re.IGNORECASE)
-        if search:
-            api_call = search.group(0).replace('aws','').strip()
-            resource_creations.append('.'.join(api_call.split()))
+    PATTERNS = [
+        r'\baws\s+\w+\s+(\b(create|put|run|make|build|launch)\b)(-\w+)+',
+        r'\baws\s+(\b(ec2|ecs|eks|lambda|s3|rds|dynamodb|cloudformation|iam)\b)\s+\w+(-\w+)*',
+        r'\baws\s+(\b(ec2|ecs|eks|lambda|s3|rds|dynamodb|cloudformation|iam)\b)\s+(\b(describe|get|list)\b)(-\w+)*',
+        r'\baws\s+(\b(ec2|ecs|eks|lambda|s3|rds|dynamodb|cloudformation|iam)\b)\s+(\b(start|stop|terminate|delete|remove)\b)(-\w+)*',
+        r'\baws\s+(\b(ec2|ecs|eks|lambda|s3|rds|dynamodb|cloudformation|iam)\b)\s+(\b(update|modify|attach|detach)\b)(-\w+)*',
+        r'\baws\s+(\b(ec2|ecs|eks|lambda|s3|rds|dynamodb|cloudformation|iam)\b)\s+(\b(create|put|run|make|build|launch)\b)(-\w+)*'
+    ]
+
+    for pattern in PATTERNS:
+        for line in file_content.split('\n'):
+            search = re.search(pattern, line, re.IGNORECASE)
+            if search:
+                api_call = search.group(0).replace('aws', '').strip()
+                resource_creations.append('.'.join(api_call.split()))
+
     return resource_creations
 
 if __name__ == '__main__':
