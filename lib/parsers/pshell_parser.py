13e9eb1569a6974a69198bf14cc0c16f7bd7349d
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-01 10:14:59 -0700

    Update pshell_parser.py

--- lib/parsers/pshell_parser.py
+++ lib/parsers/pshell_parser.py
@@ -1,6 +1,3 @@
-'''
-Powershell parser
-'''
 import boto3
 import re
 
@@ -9,20 +6,27 @@ def get_aws_service_names():
     available_services = session.get_available_services()
     return [service.replace(' ', '') for service in available_services]
 
-
 def parse_powershell_file(file_contents: str):
     # Fetch AWS service names
     service_names = get_aws_service_names()
     service_regex_part = '|'.join(service_names)
-    cmdlet_pattern = rf'\b(New|Get|Set|Delete|Update|List)-(({service_regex_part})\w*)\b'
-    # Find cmdlets using the generated regex
-    found_cmdlets = re.findall(cmdlet_pattern, file_contents, re.IGNORECASE)
+
+    cmdlet_patterns = [
+        rf'\b(New|Get|Set|Delete|Update|List)-(({service_regex_part})\w*)\b',
+        rf'\b(Start|Stop|Restart|Remove|Enable|Disable)-(({service_regex_part})\w*)\b',
+        rf'\b(Invoke|Send|Publish|Register|Unregister)-(({service_regex_part})\w*)\b',
+        rf'\b(Import|Export)-(({service_regex_part})\w*)\b',
+        rf'\b(Grant|Revoke)-(({service_regex_part})\w*)\b',
+        rf'\bAWS-(({service_regex_part})\w*)\b',
+    ]
 
     resources = []
-    for cmdlet in found_cmdlets:
-        resources.append('-'.join(cmdlet[:-1]))
-    return resources
+    for pattern in cmdlet_patterns:
+        found_cmdlets = re.findall(pattern, file_contents, re.IGNORECASE)
+        for cmdlet in found_cmdlets:
+            resources.append('-'.join(cmdlet[:-1]))
 
+    return resources
 
 if __name__ == '__main__':
     script_path = r'resources/aws.ps1'
