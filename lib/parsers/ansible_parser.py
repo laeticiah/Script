6087d4bde06a094603a79306daced8bd241c8d3b
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-03 13:08:44 -0700

    Update ansible_parser.py

--- lib/parsers/ansible_parser.py
+++ lib/parsers/ansible_parser.py
@@ -4,6 +4,9 @@ Ansible parser
 import re
 from collections import defaultdict
 import yaml  # type: ignore
+from lib.logger import setup_logger
+
+logger = setup_logger(__name__)
 
 
 def parse_ansible_file(file_content):
@@ -12,10 +15,17 @@ def parse_ansible_file(file_content):
     try:
         parsed_yaml = yaml.safe_load(file_content)
     except yaml.YAMLError as e:
-        print(f"Error parsing file: {e}")
-        return None
+        logger.error("Error parsing Ansible yaml file:%s", str(e))
+        return {}
 
-    search_terms = ["aws", "ec2", "s3", "iam", "sts", "lambda"]
+    search_terms = [
+        "aws", "ec2", "s3", "iam", "sts", "lambda", "rds", "dynamodb", "ecs",
+        "eks", "elasticache", "cloudformation", "cloudwatch", "route53", "sns",
+        "sqs", "kms", "secretsmanager", "cognito", "apigateway", "acm", "elb",
+        "elbv2", "autoscaling", "cloudfront", "cloudtrail", "config", "backup",
+        "guardduty", "inspector", "macie", "securityhub", "ssm", "stepfunctions",
+        "waf", "wafv2", "xray"
+    ]
 
     # Build the regex pattern from the search terms
     module_pattern = r"|".join(f"(?i){term}" for term in search_terms)
@@ -24,13 +34,17 @@ def parse_ansible_file(file_content):
     aws_module_counts = defaultdict(int)
 
     # Traverse the parsed content for tasks using AWS modules
-    for play in parsed_yaml:
-        if "tasks" in play:
-            for task in play["tasks"]:
-                for task_name, _ in task.items():
-                    # Check if the task name matches any search term
-                    if re.search(module_pattern, task_name):
-                        aws_module_counts[task_name] += 1
+    try:  # pylint: disable=too-many-nested-blocks
+        for play in parsed_yaml:
+            if "tasks" in play:
+                for task in play["tasks"]:
+                    for task_name, _ in task.items():
+                        # Check if the task name matches any search term
+                        if re.search(module_pattern, task_name):
+                            aws_module_counts[task_name] += 1
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        logger.error("Error parsing Ansible file: %s", str(e))
+        return {}
 
     # Convert the defaultdict to a regular dict for return
     aws_module_counts = dict(aws_module_counts)
