1f1b11cf441445fde48176273967c874363cb654
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-06 18:21:56 -0700

    Update chef_parser.py

--- lib/parsers/chef_parser.py
+++ lib/parsers/chef_parser.py
@@ -1,1 +1,41 @@
+import re
+import os
+from lib.logger import setup_logger
 
+logger = setup_logger(__name__)
+def parse_chef_file(file_content):
+    # Regex pattern to initially capture potential AWS SDK calls
+    pattern = re.compile(r'\b(\w+)\.(\w+)\(')
+    #pattern = re.compile(r'\b(?:aws_|AWS::)?\w+\.(?:\w+_)?\w+')
+
+    # List of known non-AWS calls to exclude from results
+    non_aws_methods = {
+        'Log.debug', 'Log.info', 'Timeout.timeout', 'URI.unescape', 'end.run_action', 'new_resource.snapshot_id'
+    }
+
+    # Dictionary to store AWS-specific results, keyed by "ServiceName.MethodName"
+    resources_used = {}
+
+    # Find all matches and filter non-AWS calls
+    for _match in re.finditer(pattern, file_content):
+        key = f"{_match.group(1)}.{_match.group(2)}"
+        if key not in non_aws_methods:
+            if key not in resources_used:
+                resources_used[key] = 0
+            resources_used[key] += 1
+
+    return resources_used
+
+# Re-analyzing with the revised approach to include S3
+all_resources = {}
+if __name__ == '__main__':
+    files = ['elastic_ip.rb','ebs_volume.rb','iam_policy.rb','instance_role.rb','s3_bucket.rb']
+    for file in files:
+        content = open(os.path.join('resources', 'chef', file)).read()
+        resources = parse_chef_file(content)
+        for key, value in resources.items():
+            if key in all_resources:
+                all_resources[key] += value
+            else:
+                all_resources[key] = value
+        print(file, '==>', all_resources)
