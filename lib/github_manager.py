0c9f5d961d72aca375ff2a6b1d1cb3623eadb7c6
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-05-09 10:22:52 -0700

    Update github_manager.py

--- lib/github_manager.py
+++ lib/github_manager.py
@@ -51,7 +51,12 @@ def extract_files_from_repo(repo: Repository.Repository) -> List[ContentFile.Con
    """
    matched_files = []
    try:
-       queue = collections.deque([repo.get_contents("")])
+       contents = repo.get_contents("")
+       if contents is None:
+           logger.warning("Repository '%s' is empty or cannot be accessed.", repo.full_name)
+           return matched_files
+
+       queue = collections.deque([contents])
 
        while queue:
            file_content: Union[List[ContentFile.ContentFile], ContentFile.ContentFile] = queue.popleft()
@@ -145,6 +150,7 @@ def get_repo_metadata(a_repo: Repository.Repository) -> dict[str, Any]:
 
    return min_metadata
 
+
 def retrieve_repos(github_client: Github, user_or_org: Optional[str] = None, repository: Optional[str] = None) -> List[Repository.Repository]:
     """
     Retrieves repositories from organizations within the GitHub Enterprise Managed User (EMU).
@@ -164,10 +170,10 @@ def retrieve_repos(github_client: Github, user_or_org: Optional[str] = None, rep
             if "/" in user_or_org:  # Handle organization format (username/org)
                 org_name = user_or_org.split("/")[1]
                 org = github_client.get_organization(org_name)
-                repos.extend(get_org_repos(org, repository))
+                repos = get_org_repos(org, repository)
             else:
                 org = github_client.get_organization(user_or_org)
-                repos.extend(get_org_repos(org, repository))
+                repos = get_org_repos(org, repository)
         else:
             for org in github_client.get_organizations():
                 repos.extend(get_org_repos(org, repository))
@@ -179,8 +185,11 @@ def retrieve_repos(github_client: Github, user_or_org: Optional[str] = None, rep
         logger.info("Found %s repositories:", len(repos))
         for repo in repos:
             logger.info("\t- %s", repo.full_name)
+    else:
+        logger.warning("No repositories found.")
 
     return repos
+
 
 def get_org_repos(org: Organization.Organization, repository: Optional[str] = None) -> List[Repository.Repository]:
     """
@@ -198,33 +207,6 @@ def get_org_repos(org: Organization.Organization, repository: Optional[str] = No
     else:
         return list(org.get_repos(type='all'))
 
-# def retrieve_repos(github_client: Github, user_or_org: str, repository: Optional[str]) -> List[Repository.Repository]:
-#    """Retrieves repositories based on user/org (paginated)."""
-#    repos = []
-#    try:
-#        if "/" in user_or_org:  # Handle organization format (username/org)
-#            org = user_or_org.split("/")
-#            if repository:
-#                repos.append(github_client.get_organization(org).get_repo(repository))
-#            else:
-#                for repo in github_client.get_organization(org).get_repos():
-#                    repos.append(repo)  # Append individual repositories
-#        else:
-#            if repository:
-#                repos.append(github_client.get_user(user_or_org).get_repo(repository))
-#            else:
-#                for repo in github_client.get_user().get_repos():
-#                    repos.append(repo)  # Append individual repositories
-#    except GithubException as e:
-#        logger.error("Error retrieving repos for %s: %s", user_or_org, e)
-
-#    if repos:
-#        logger.info("Found %s repositories:", len(repos))
-#        for repo in repos:
-#            logger.info("\t- %s", repo.full_name)
-
-#    return repos
-
 
 def analyze_repo(repo: Repository.Repository, match_functions: list[dict, Any], output_file):
    """ Analyzes a single Github repository based on provided match functions.
@@ -240,29 +222,24 @@ def analyze_repo(repo: Repository.Repository, match_functions: list[dict, Any], 
 
    logger.debug("match_function: %s", match_functions)
 
-   try:
-       contents = repo.get_contents("")
-       if not contents:
-           logger.warning("Repository '%s' is empty. Skipping analysis.", repo.full_name)
-           return
-   except GithubException as e:
-       logger.error("Error accessing repository '%s': %s", repo.full_name, e)
-       return
-
    # Extract metadata
-   branch_metadata: dict[str, Any]
+   branch_metadata: dict[str, Any] = {}
    try:
        branch_metadata = get_repo_metadata(a_repo=repo)
    except GithubException as e:
        logger.error("Error getting GitHub metadata. Repository '%s'. Error: %s", repo.full_name, e)
-       branch_metadata = {}
 
    # Open CSV file (modify based on your CSV handling logic)
    with open(output_file, 'a', newline='', encoding='utf-8') as csvfile:
        writer = csv.writer(csvfile)
 
        # Loop over fetched files and configurations
-       for file_content in extract_files_from_repo(repo):
+       files = extract_files_from_repo(repo)
+       if not files:
+           logger.warning("No files found in repository '%s'. Skipping analysis.", repo.full_name)
+           return
+
+       for file_content in files:
            # logger.info("-----------------------------------------")
            logger.info("Analyzing file --> %s", f"{repo.full_name}/{file_content.path}")
            # logger.info("-----------------------------------------")
